78952db1b22f1b0471fbc845ed7c1bb4acc6bc5d
Rishi Joshi <110519406+tangorishi@users.noreply.github.com>
2023-04-06 09:38:43 -0700

    Update longlegs.cpp

--- contest2/longlegs.cpp
+++ contest2/longlegs.cpp
@@ -1,16 +1,35 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define ll long long
 using namespace std;
-
-int main()
+int finds(int l)
 {
+    for(int i=2;i*i<=l;i++)
+    {
+        if(l%i==0)
+        {
+            return i;
+        }
+    }
+    return 1;
+}
 
+void solve()     
+{
+    int a,b;
+    cin>>a>>b;
+    ll x = __gcd(a,b);
+    if(x>1)
+    {
+        cout<<x-1+a/x+b/x<<endl;
+        return;
+    }
+    cout<<min(a,b)+finds(max(a,b))-1+max(a,b)/finds(max(a,b))<<endl;
+    }
+
+int main(){
     int t;
     cin >> t;
-    while (t--)
-    {
-        int a, b;
-        cin >> a >> b;
-        cout<< min(a-b % pow(a, b)) << "\n";
+    while(t--){
+        solve();
     }
-    return 0;
 }
