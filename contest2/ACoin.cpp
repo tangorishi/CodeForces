9f63979212e02f464f10602a57c1dc668e659b9c
Rishi Joshi <110519406+tangorishi@users.noreply.github.com>
2023-04-06 22:08:24 +0530

    Add files via upload

---
+++ contest2/ACoin.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h> 
+using namespace std;
+#define ll long long 
+#define MOD 1000000007
+#define SIZE 1e6 + 2
+#define endl "\n"
+void solve()
+{
+
+ll n, k;
+if (k % 2 == 0 and n % 2)
+    cout << "NO" << endl;
+else
+    cout << "YES" << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
